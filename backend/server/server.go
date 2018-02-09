158e15279a80943ffb7472ff0c6dc5d08f42268e
Luca Moser <moser.luca@gmail.com>
2018-02-09 16:55:25 +0100

    auto open browser up on startup

--- backend/server/server.go
+++ backend/server/server.go
@@ -13,7 +13,7 @@ import (
 	"os"
 	"time"
 	"context"
-	//"github.com/skratchdot/open-golang/open"
+	"github.com/skratchdot/open-golang/open"
 )
 
 type TemplateRendered struct {
@@ -128,7 +128,7 @@ func (server *Server) Start() {
 	go e.Start(httpConfig.Address)
 
 
-	//open.Start("http://localhost:9090")
+	open.Start("http://localhost:9090")
 
 	// finish
 	delta := (time.Now().UnixNano() - start) / 1000000
